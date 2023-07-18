25e6d389042c8bf80af19c06e8bf510d49892e19
niming1011 <s2915055618@Outlook.com>
2023-07-18 16:23:18 +0800

    练习代码

---
+++ test1.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+int main() {
+	////1!+2£°+...+n!
+	//int i = 1,n,t=1,m=0;
+	//printf("«Î ‰»În");
+	//scanf("%d", &n);
+	//for (i = 1; i <= n; i++)
+	//{
+	//	t *= i;
+	//	m += t;
+	//}
+	//printf("%d\n", m);
+	/*printf("%03d", 5);*/
+	//int n, x=50, y=100;
+	//while (n = x % y) {
+	//	y=n;
+	//	x = y;
+
+	//}
+	//printf("%d", y);
+	//char arr[10000]="abcdef";
+	//
+	//gets_s(arr);
+	//int left, right;
+	//left = 0;
+	//right = (strlen(arr) - 1);
+	//char t;
+
+	//while (left < right) {
+	//	t = arr[left];
+	//	arr[left] = arr[right];
+	//	arr[right] = t;
+	//	left++;
+	//	right--;
+
+	//}
+	//printf("%s", arr);
+	//  int a = -1;
+	//printf("%ud", a);
+	int i = 1;
+	if (*(char*)&i == 1) {
+		printf("¥Û∂À");
+
+	}
+	else
+		printf("–°∂À");
+
+
+	return 0;
+}
